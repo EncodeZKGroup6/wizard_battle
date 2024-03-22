5b054ccd5def530bc99904f2d71c118985a2d5d0
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-03-23 00:49:49 +0300

    Initial contract and readme done

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
-# Mina zkApp: Wizard_battle
+# Wizzard battle
 
-This template uses TypeScript.
+Wizzard battle is a ZK game that was created during Encode ZK Bootcamp project by Group 6.
+
+## Rules
+
+- Each wizard live within a field 4x4
+- Each wizard have 3 health. When health goes to zero he dies. Health is public
+- Each wizard have a spells. He can choose 2 before match starts
+- There is only one winner
+
+## State model
+
+- **Init** Users public keys set
+- **Setup** Users inital state set + InitialStateProof check
+- **Action Commit** At this stage users send commitment(hash of action + salt) of their actions for the next turn + proof of commitment
+- **Action Reveal** At this stage users send actions, that were commited earlier
+- **Action Apply** At this stage users send proofs of their health after all actions applying. If one or less wizard alive, goes to **End** phaze, otherwize goes to **Action Comit**
+- **End**
+
+![plot](./img/wizard_battle_diagram.jpg)
+
+## Data structures
+
+We are using Mina, so we are seriously limited with space we have.
+
+1. Merkle root of user public keys (hashed)
+2. Stage
+3. Player1 Status. Players statuses also can be stored as merkle tree, if needed. This will free 3 more slots.
+4. Player2 Status
+5. Player3 Status
+6. Player4 Status
+7. Game data. Number of alive wizards, number of actions left, current actions - all packed into single field
+8.
+
+## Proofs
+
+- **Initial state proof**. Proof that position and spels is correct
+- **Commitment proof**. Proof that player can perform action that he is commiting
+- **Action apply proof**. Proof of health after round
 
 ## How to build
 
--- src/WizardBattle.ts
+++ src/WizardBattle.ts
@@ -1,14 +1,24 @@
-import { Field, SmartContract, state, State, method, PublicKey } from 'o1js';
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  PublicKey,
+  Provable,
+  UInt64,
+} from 'o1js';
 
 const GameStatus = {
   init: Field.from(0),
   prep: Field.from(1),
-  spellCommiment: Field.from(2),
+  spellCommit: Field.from(2),
   spellReveal: Field.from(3),
-  finished: Field.from(4),
+  spellApply: Field.from(4),
+  finished: Field.from(5),
 };
 
-export class Square extends SmartContract {
+export class WizardBattle extends SmartContract {
   // Merkle tree of players
   @state(Field) players = State<Field>();
 
@@ -20,7 +30,7 @@ export class Square extends SmartContract {
   @state(Field) player3Status = State<Field>();
   @state(Field) player4Status = State<Field>();
 
-  @state(Field) waitLeft = State<Field>();
+  @state(UInt64) waitLeft = State<UInt64>();
 
   @method setup(playersRoot: Field) {
     const curStatus = this.status.getAndRequireEquals();
@@ -29,20 +39,41 @@ export class Square extends SmartContract {
     this.status.set(GameStatus.prep);
   }
 
+  /*
+    In this stage player sending their initial state with a proof, that this initial state is correct
+  */
   @method setPlayerState(state: Field, proofOfState: ProofOfState) {
     const curStatus = this.status.getAndRequireEquals();
     curStatus.assertEquals(GameStatus.prep);
 
     proofOfState.verify();
+
+    // Verify, that sender is player
+
+    const waitLeft = this.waitLeft.getAndRequireEquals();
+    const prepFinished = waitLeft.equals(UInt64.from(1));
+    this.waitLeft.set(Provable.if(prepFinished, UInt64.from(4), waitLeft));
+    this.status.set(
+      Provable.if(
+        prepFinished,
+        GameStatus.spellCommit,
+        this.status.getAndRequireEquals()
+      )
+    );
   }
 
-  @method commitMove() {
+  @method commitMove(commit: Field, commitProof: ProofOfCommit) {
     const curStatus = this.status.getAndRequireEquals();
-    curStatus.assertEquals(GameStatus.spellCommiment);
+    curStatus.assertEquals(GameStatus.spellCommit);
   }
 
-  @method revealMove() {
+  @method revealMove(value: Field) {
     const curStatus = this.status.getAndRequireEquals();
     curStatus.assertEquals(GameStatus.spellReveal);
+  }
+
+  @method apply(appyProof: ProofOfApply) {
+    const curStatus = this.status.getAndRequireEquals();
+    curStatus.assertEquals(GameStatus.spellApply);
   }
 }
