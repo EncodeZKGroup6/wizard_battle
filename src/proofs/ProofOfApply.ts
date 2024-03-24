a4a7fc82985fb1a8004cd1545447e23dcc2f066b
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-03-24 18:32:32 +0300

    Added description for ProofOfApply

--- src/proofs/ProofOfApply.ts
+++ src/proofs/ProofOfApply.ts
@@ -1,11 +1,27 @@
-import { Struct, ZkProgram } from 'o1js';
+import { Field, Struct, ZkProgram } from 'o1js';
 
-export class ApplyPublicInput extends Struct({}) {}
+export class ApplyPublicInput extends Struct({
+  actions: Field,
+  state: Field,
+}) {}
 
-export class ApplyPublicOutput extends Struct({}) {}
+export class ApplyPublicOutput extends Struct({
+  health: Field,
+}) {}
 
-const proveApply = (publicInput: ApplyPublicInput): ApplyPublicOutput => {
-  return new ApplyPublicOutput({});
+/*
+    This one should apply all spells from actions and return final health of wizard
+*/
+const proveApply = (
+  publicInput: ApplyPublicInput,
+  spell1: Field,
+  spell2: Field,
+  posX: Field,
+  posY: Field
+): ApplyPublicOutput => {
+  return new ApplyPublicOutput({
+    health: Field.from(0),
+  });
 };
 
 export const ApplyProgramm = ZkProgram({
@@ -15,7 +31,7 @@ export const ApplyProgramm = ZkProgram({
 
   methods: {
     proveApply: {
-      privateInputs: [],
+      privateInputs: [Field, Field, Field, Field],
       method: proveApply,
     },
   },
