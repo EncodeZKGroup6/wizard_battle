25d55d3fd5bd602c41e93c1bc8c008d4f53afa81
Lander Elizondo <48693291+LanderElizondo@users.noreply.github.com>
2024-03-26 19:04:24 +0100

    Update ProofOfState.ts
    Additional drafts

--- src/proofs/ProofOfState.ts
+++ src/proofs/ProofOfState.ts
@@ -202,6 +202,151 @@ class BoardCircuit {
         
         return boardHash;
     }
+  
+  class AttackUtils { 
+    /**
+     * Parse a target coordinate pair into Field elements.
+     * @param {number[]} target - The x/y coordinate pair representing the target.
+     * @returns {Field[]} The parsed target coordinates as Field elements.
+     */
+    static parseTarget(target: number[]) {
+        return target.map(Field);
+    }
+
+    /**
+     * Serialize a target coordinate pair for attacking.
+     * @param {number[]} target - The x/y coordinate pair representing the target.
+     * @returns {Field} The serialized target as a Field element.
+     */
+    static serializeTarget(target: number[]) { 
+        const parsedTarget = AttackUtils.parseTarget(target);
+
+        const xBits = parsedTarget[0].toBits(4);
+        const yBits = parsedTarget[1].toBits(4);
+        const serializedTarget = Field.fromBits([...xBits, ...yBits]);
+
+        return serializedTarget;
+    }
+
+    /**
+     * Deserialize a serialized target.
+     * @param {Field} serializedTarget - The serialized target as a Field elements.
+     * @returns {Field[]} The deserialized x/y coordinate pair.
+     */
+    static deserializeTarget(serializedTarget: Field) { 
+        const bits = serializedTarget.toBits(8);
+        const targetX = Field.fromBits(bits.slice(0, 4));
+        const targetY = Field.fromBits(bits.slice(4, 8));
+
+        return [targetX, targetY];
+    }
+
+    /**
+     * Validate a target falls within the game map range.
+     * Throws an error if the target is out of bounds.
+     * @param {Field} serializedTarget - The target as an array of two Field elements.
+     */
+    static validateTarget(target: Field[]) {
+        target[0].assertLessThan(4, 'Target x coordinate is out of bound!');
+        target[1].assertLessThan(4, 'Target y coordinate is out of bound!');
+    }
+
+    /**
+     * Validate a serialized target to ensure it falls within the game map range after deserialization.
+     * Throws an error if the target is out of bounds.
+     * @param {Field} serializedTarget - The serialized target as a Field element.
+     */
+    static validateSerializedTarget(serializedTarget: Field) { 
+        const target = AttackUtils.deserializeTarget(serializedTarget);
+        AttackUtils.validateTarget(target);
+    }    
+
+    /**
+     * Serialize hit count history into a single Field object.
+     * @param {Field[]} hitHistory - An array containing hit count history for both players.
+     * @returns {Field} The serialized hit count history as a Field object.
+     */
+    static healthCheck(health: Field[]) {
+        // Extract hit counts for player 1 and player 2
+        const [player1Health, player2Health, player3Health, player2Health] = health;
+        
+        // Convert hit counts to bit representations
+        const player1HealthBits = player1Health,.toBits(2);
+        const player2HealthBits = player2Health,.toBits(2);
+        const player3HealthBits = player3Health,.toBits(2);
+        const player4HealthBits = player4Health,.toBits(2);
+
+        // Concatenate bit representations and create a single Field object
+        const healthCheck = Field.fromBits([...player1HealthBits, ...player2HealthBits, ...player3HealthBits, ...player4HealthBits]);
+        
+        return healthCheck;
+    }
+
+    /**
+     * Encode hit target coordinates into a single field element for storage efficiency.
+     * 
+     * We encode data differently than `serializeTarget` to showcase an alternative approach to conserve on-chain storage space.
+     * When bitifying two numbers from 0 to 9 together, it typically requires 8 bits in total. However, by employing a technique
+     * like bitifying an encode variable such as 9 + 9*10 + 1, we reduce the storage requirement to 7 bits. This results in a total saving of 34 bits
+     * because we store a maximum of 34 targets that land a successful hit.
+     * 
+     * Serializing an encoded target saved 34 bits of storage otherwise we would have exceed the field element size.
+     * 
+     * Encoding is achieved by mapping a target [x, y] to a single field element: x + 10 * y + 1.
+     * To distinguish encoded hit targets from initial values, we add one to the result. For example, if [0, 0] represents 
+     * a target that successfully hits the adversary, serializing it would result in 0, which is indistinguishable from 
+     * the initial value(0) and prone to errors. Therefore, we add one to the encoded value to avoid confusion and ensure 
+     * proper differentiation.
+     * 
+     * @param {Field[]} hitTarget - The coordinates of the hit target [x, y].
+     * @returns {Field} The encoded hit target.
+     * @note hit target refers to a player's target that resulted in a successful hit
+     */
+    static encodeHitTarget(hitTarget: Field[]) { 
+        const encodeHitTarget = hitTarget[0].add(hitTarget[1].mul(10)).add(1);
+        return encodeHitTarget;
+    }
+}
+
+class AttackCircuit {
+    /**
+     * Determine whether or not a given ship is hit by a given adversary target.
+     * @param {Field[]} target - The x/y coordinate pair representing the adversary target.
+     * @param {Field[]} ship - The wizard coordinates.
+     * @returns {Bool} True if the wizard is hit, false otherwise.
+     */
+    static scanWizard(target: Field[], wizard: Field[]) { 
+        // Return hit result for a horizontal ship
+        const hitWizard = () => {
+            let xHit = Bool(false);
+            let yHit = Bool(false);
+            xHit = xHit.or(wizard[0].equals(target[0]));
+            yHit = yHit.or(wizard[1].equals(target[1]));
+    
+            return xHit.and(yHit);
+        }
+        
+
+        // True if hit, false if missed
+        const hitResult = Provable.if(wizard[2].equals(1), hitWizard());
+        return hitResult;
+    }
+
+    /**
+     * Determine whether or not a target hits a given board arrangement.
+     * @param {Field[][]} ships - The coordinates of all wizard on the board.
+     * @param {Field[]} target - The x/y coordinate pair representing the adversary target.
+     * @returns {Bool} True if the target wizard a ship, false otherwise.
+     */
+    static attack(wizard: Field[][], target: Field[]) { 
+        // Assert that the target is within board range
+        AttackUtils.validateTarget(target);
+
+        let hit = Bool(false);
+        hit = hit.or(AttackCircuit.scanWizard(target, wizard));
+      
+        return hit;
+    }
 }
 
 export class ProofOfState extends ZkProgram.Proof(StateProgramm) {}
